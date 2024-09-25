02c67fb80e2ec07649d2162cc86c40a2bf55984e
dishavanani13 <dishvanani13@gmail.com>
2024-09-24 21:57:19 -0400

    back to mobile app

--- HackerApp/Models/WebView.swift
+++ HackerApp/Models/WebView.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import WebKit
+import SwiftUI
+
+struct Webview :  UIViewRepresentable { //  ,  { NSViewRepresentable
+
+ 
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeURL = urlString {
+            if let urll = URL(string: safeURL){
+                let request = URLRequest(url: urll)
+                uiView.load(request)
+            }
+        }
+    }
+//    func makeNSView(context: Context) -> WKWebView {
+//        return WKWebView()
+//    }
+//    
+//    func updateNSView(_ nsView: WKWebView, context: Context) {
+//        if let safeURL = urlString {
+//            if let urll = URL(string: safeURL){
+//                let request = URLRequest(url: urll)
+//                print(request)
+//               
+//                nsView.load(request)
+//            }
+//        }
+//    }
+    
+   
+    
+    
+}
+
+
